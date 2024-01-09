f1698275e347ace48446fe54809a3ddefdf1e92d
diffident016 <marvin.tagolimotjr13@gmail.com>
2024-01-09 09:04:12 +0800

    feat: added mongoDBURI as env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 require("dotenv").config();
 
-const URI = "mongodb+srv://diffident016:hypermode18@cluster0.ewe91.mongodb.net/randomquotes?retryWrites=true&w=majority"
-
-const connection = mongoose.connect(process.env.mongoUrl || URI);
+const connection = mongoose.connect(process.env.mongoUrl);
 
 module.exports = { connection };
